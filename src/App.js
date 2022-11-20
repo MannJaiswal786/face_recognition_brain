0e5495d1003f1de505ff4504cad49283b618c0ed
Mann Jaiswal <86150331+MannJaiswal786@users.noreply.github.com>
2022-11-20 16:21:45 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -33,13 +33,49 @@ onButtonSubmit = () => {
   this.state.input)
   .then(
 function(response){
-  console.log(response);
+  console.log(response.outputs[0].region_info.bounding_box);
 },
 function(err){
 // there was an error
 }
     );
 }
+
+
+ /* onButtonSubmit = () => {
+   const raw = JSON.stringify({
+     user_app_id : {
+       user_id: "26mo438js0i7",
+       app_id: "my-first-application"
+     },
+     inputs: [
+       {
+         data: {
+           image: {
+             url: this.state.input
+           },
+         },
+       },
+     ],
+   });
+
+   fetch(
+      "https://api.clarifai.com/v2/models/f76196b43bbd45c99b4f3cd8e8b40a8a/outputs",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          Authorization: "03bb36287d8f4df497165cc4b9738e5e",
+        },
+        body: raw,
+      }
+    )
+    .then((response) => response.text())
+    .then((result) => this.displayFaceBox(this.calculateFaceLocation(result)))
+    .catch((error) => console.log("error", error));
+ }
+ */
+
 
   render() {
   return (
